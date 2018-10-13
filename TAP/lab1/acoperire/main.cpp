26a7d2ebd7ed5e61385044b4ab45d73f7b109224
Iuga Stefan <iugastefan@icloud.com>
2018-10-13 11:30:50 +0300

    Revert "Revert "adaugat lab2 pentru TAP""
    This reverts commit 030f8ee921d2fde16192ff0e8cf8098e51b1c479.

---
+++ TAP/lab1/acoperire/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int citire(int ceva) { return ceva + 2; }
+
+int main() {
+  std::cout << "omega";
+  std::cout << "\nlul";
+  std::string mesaj;
+  std::cin >> mesaj;
+  std::cout << mesaj;
+  return 0;
+}
