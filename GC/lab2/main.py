6b9047b18fbc42fd2cb0739f9e110ed7392d5fc8
Iuga Stefan <iugastefan@icloud.com>
2018-10-17 10:24:39 +0300

    GC: lab2: alta incercare main.py

--- GC/lab2/main.py
+++ GC/lab2/main.py
@@ -1,4 +1,4 @@
-class punct(object):
+class point(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -12,8 +12,31 @@ class punct(object):
         if self.y <= p2.y and self.x < p2.x:
             return -1
         return 1
+def dist(p1:point, p2:point):
+    val=(p1.x-p2.x)^2+(p1.y)^2
+    return val
+def orientation(p1:point,p2:point ,p3:point):
+    val = (p2.y-p1.y)*(p3.x-p2.x)-(p2.x-p1.x)*(p3.y-p2.y)
+    if val==0:
+        return 0
+    if val>0:
+        return 1
+    else:
+        return -1
 
+def compare(p1:point, p2:point):
+    p0 = point(0,0)
+    orient = orientation(p0,p1,p2)
+    if orient == 0:
+        if dist(p0,p2)<dist(p0,p1):
+            return True
+        else:
+            return False
+    if orient == 1:
+        return True
+    else:
+        return False
 
-v = [punct(-1, -2), punct(-1, 2), punct(1, 2), punct(1, -2), punct(0, 0)]
+v = [point(-1, -2), point(-1, 2), point(1, 2), point(1, -2), point(0, 0)]
 v.sort()
 print(v)
