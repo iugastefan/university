3d0fd993e4abb928cb57043f485cd23cb2eef278
Iuga Stefan <iugastefan@icloud.com>
2018-10-18 13:20:05 +0300

    TAP: lab2: profit: refacut, aproape gata

--- TAP/lab2/profit/main.cpp
+++ TAP/lab2/profit/main.cpp
@@ -1,11 +1,18 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <queue>
 #include <vector>
 using namespace std;
 struct activitate {
   int profit, termen, ordine;
 };
+struct minim {
+  int val;
+  list<activitate>::iterator it;
+  bool operator()(const minim &a, const minim &b) { return a.val > b.val; }
+};
+
 int main() {
   vector<activitate> activitati{
       {100, 4, 1}, {199, 1, 2}, {27, 2, 3}, {25, 2, 4}, {150, 4, 5}};
@@ -18,30 +25,39 @@ int main() {
        });
   int sum = 0;
   list<activitate> orar;
+  priority_queue<minim, vector<minim>, minim> mini;
   for (auto x : activitati) {
     if (orar.empty()) {
       orar.push_back(x);
+      auto ptr = orar.end();
+      ptr--;
+      mini.push(minim{x.profit, ptr});
       sum += x.profit;
     } else {
       if (orar.back().termen < x.termen) {
         orar.push_back(x);
+        auto ptr = orar.end();
+        ptr--;
+        mini.push(minim{x.profit, ptr});
         sum += x.profit;
       } else {
-        if (x.profit > orar.front().profit) {
-          orar.push_back(x);
-          sum += x.profit;
-          sum -= orar.front().profit;
-          orar.pop_front();
-        } else {
-          sum -= orar.back().profit;
-          orar.pop_back();
-          orar.push_back(x);
-          sum += x.profit;
-        }
+        sum -= mini.top().val;
+        orar.erase(mini.top().it);
+        mini.pop();
+        orar.push_back(x);
+        auto ptr = orar.end();
+        ptr--;
+        mini.push(minim{x.profit, ptr});
+        sum += x.profit;
       }
     }
   }
   cout << sum << endl;
   for (auto x : orar)
     cout << x.ordine << " ";
+  cout << endl;
+  while (!mini.empty()) {
+    cout << mini.top().val << " ";
+    mini.pop();
+  }
 }
