44843a5ac93a37716516b36ec341dda6c420022d
Iuga Stefan <iugastefan@icloud.com>
2018-11-03 22:35:48 +0200

    SO: lab6: terminat ex 1, adaugat makefile

---
+++ SO/lab6/strrev.c
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void *reverse(void *v) {
+  char *word = (char *)v;
+  ulong size = strlen(word);
+  char *rword = (char *)malloc(size * sizeof(char));
+  int i;
+  for (i = 1; i <= (int)size; i++)
+    rword[size - i] = word[i - 1];
+  return rword;
+}
+int main(int argc, char **argv) {
+  pthread_t thr;
+  if (pthread_create(&thr, NULL, reverse, argv[1])) {
+    perror(NULL);
+    return errno;
+  }
+  char *rword;
+  if (pthread_join(thr, (void **)&rword)) {
+    perror(NULL);
+    return errno;
+  }
+  printf("%s", rword);
+  return 0;
+}
